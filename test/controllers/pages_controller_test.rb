0b08661a53cc26afbe6def2af202cc71db0d0111
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-09 03:12:44 +0600

    basic design

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,6 +5,8 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
 
     assert_response :success
-    assert_select "h1", text: /Build, manage, and grow/
+    assert_select "h1", text: /Build\. Market\. Automate\. Grow\./
+    assert_select "img[alt='M&W Labs digital services']"
+    assert_select "img[alt='Shopify']"
   end
 end
