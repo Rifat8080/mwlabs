3738ba2b36ab8832cd9ac23abdede9f25558b91f
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-10 12:29:02 +0600

    fix designs hero section

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -8,6 +8,7 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", text: /Build\. Market\. Automate\. Grow\./
     assert_select "img[alt='M&W Labs digital services']"
     assert_select "img[alt='Shopify']"
+    assert_select "img[alt='Tech To The Rescue']"
     assert_select "h2", text: /Everything Your Business Needs/
     assert_select ".fa-code"
   end
